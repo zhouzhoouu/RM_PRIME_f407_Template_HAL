8f39b856f3e22fc801dd4b376b5357415cfd4737
Z.Zhou <8520897+zzhouJOJO@user.noreply.gitee.com>
2026-03-10 08:27:34 +0800

    添加自定义ui

---
+++ CppResource/Component/UI_bismarckkk/ui.h
@@ -0,0 +1,24 @@
+//
+// Created by RM UI Designer
+// Static Edition
+//
+
+#ifndef UI_H
+#define UI_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ui_interface.h"
+
+#include "ui_g.h"
+
+void ui_init_g_Ungroup();
+void ui_update_g_Ungroup();
+void ui_remove_g_Ungroup();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UI_H
---
+++ CppResource/Component/UI_bismarckkk/ui_base/ui_interface.h
@@ -0,0 +1,26 @@
+//
+// Created by bismarckkk on 2024/2/17.
+//
+
+#ifndef UI_INTERFACE_H
+#define UI_INTERFACE_H
+
+#include <stdio.h>
+#include "ui_types.h"
+
+extern int ui_self_id;
+
+void print_message(const uint8_t* message, int length);
+void uart_send(const uint8_t* message, int length);
+
+// User Code Begin
+#define SEND_MESSAGE(message, length) uart_send(message, length)
+// User Code End
+
+void ui_proc_1_frame(ui_1_frame_t *msg);
+void ui_proc_2_frame(ui_2_frame_t *msg);
+void ui_proc_5_frame(ui_5_frame_t *msg);
+void ui_proc_7_frame(ui_7_frame_t *msg);
+void ui_proc_string_frame(ui_string_frame_t *msg);
+
+#endif //UI_INTERFACE_H
