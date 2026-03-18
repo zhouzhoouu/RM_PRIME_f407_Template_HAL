3c485c71e6a1ba2b4a8d7ac90c49f3679c657310
Z.Zhou <8520897+zzhouJOJO@user.noreply.gitee.com>
2026-03-18 15:18:35 +0800

    拨弹盘适配

--- CppResource/Tasks/ShootTask/TrigerControl.cpp
+++ CppResource/Tasks/ShootTask/TrigerControl.cpp
@@ -2,17 +2,14 @@
 
 namespace TriggerControl{
 
-    constexpr float i_tor_limit = 16000.f;
-
     uint32_t timeout = 0;
-    int dir = -1;
-    constexpr uint32_t trig_id = 2;
+    int dir = 1;
+    constexpr uint32_t trig_id = 0;
     bool isbacking = false;
 
-    float last_error = 0;
-    float integral = 0;
-
     DJiMotorGroup m3508Group_triger(&hcan1, 0x205, 0x1ff);
+
+    DeltaPID Triger_PID = DeltaPID(2.0f, 0.05f, 0.0f, 0.0f, MAX_Tri_CUR, -MAX_Tri_CUR);
 
     void setTrigSpeed(float target_speed){
 
@@ -22,12 +19,11 @@ namespace TriggerControl{
         short tmp_cur[] = {0, 0, 0, 0};
         float motor_speed = m3508Group_triger.getMotorState(trig_id).speed;
 
-        float ref = target_speed - dir*motor_speed;
-        if(ref > 200 && !isbacking){
+        float error = target_speed - dir*motor_speed;
+        if(error > 500){
             timeout++;
             if(timeout > Tri_BACKING_TIME){
                 isbacking = true;
-                integral = 0;
             }
         }
         else{
@@ -38,33 +34,8 @@ namespace TriggerControl{
 
         }
 
-        if(ref < -500){
-            integral = 0;
-        }
-
-        if(timeout < 0)
-            timeout = 0;
-
-
-
-        float error = dir*target_speed - motor_speed;
-        integral += error;
-        float i_tor = integral * 0.03f;
-
-        if(i_tor > i_tor_limit)
-            i_tor = i_tor_limit;
-        else if(i_tor < -i_tor_limit)
-            i_tor = -i_tor_limit;
-
-
-        float exp_i = error * 2. + i_tor;
-
-        if(exp_i > MAX_Tri_CUR)
-            exp_i = MAX_Tri_CUR;
-        else if(exp_i < -MAX_Tri_CUR)
-            exp_i = -MAX_Tri_CUR;
-
-        tmp_cur[trig_id] = exp_i;
+        Triger_PID.Run(dir*target_speed, motor_speed);
+        tmp_cur[trig_id] = (short)Triger_PID.Output;
         m3508Group_triger.setMotorCurrent(tmp_cur);
     }
 
