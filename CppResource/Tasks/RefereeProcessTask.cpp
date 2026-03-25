8c3b445ce51e1b5dac52ba99518f4815d7f83cac
Z.Zhou <8520897+zzhouJOJO@user.noreply.gitee.com>
2026-03-26 00:13:16 +0800

    无裁判系统电源适配

--- CppResource/Tasks/RefereeProcessTask.cpp
+++ CppResource/Tasks/RefereeProcessTask.cpp
@@ -14,6 +14,12 @@ using namespace Device;
 
         bool gimbal_on = href.getRefereeInfo<RefereeType::GameRobotState>().power_management_gimbal_output;
         bool frib_on = href.getRefereeInfo<RefereeType::GameRobotState>().power_management_shooter_output;
+
+        if(!href.RefereeExist()){
+            gimbal_on = true;
+            frib_on = true;
+        }
+
         HAL_GPIO_WritePin(Frib_Relay_GPIO_Port, Frib_Relay_Pin, frib_on ? GPIO_PIN_SET : GPIO_PIN_RESET);
         HAL_GPIO_WritePin(Pitch_Relay_GPIO_Port, Pitch_Relay_Pin, gimbal_on ? GPIO_PIN_SET : GPIO_PIN_RESET);
 
