b9d90728b0846e84785c6239184d2d047002e560
Z.Zhou <8520897+zzhouJOJO@user.noreply.gitee.com>
2026-03-26 00:07:53 +0800

    无裁判系统电源适配

--- CppResource/Tasks/MotionControlTask/MotionFSM/InitLoop.cpp
+++ CppResource/Tasks/MotionControlTask/MotionFSM/InitLoop.cpp
@@ -8,7 +8,12 @@ StateLoopArg MotionFSM::InitLoop(const volatile RCcmd_t*, INS_Device&, const Sta
 
     bool gimbal_on = hreferee.getRefereeInfo<RefereeType::GameRobotState>().power_management_gimbal_output;
     bool chassis_on = hreferee.getRefereeInfo<RefereeType::GameRobotState>().power_management_chassis_output;
-    auto sta = HAL_GPIO_ReadPin(BUTTON_TRIG_GPIO_Port, BUTTON_TRIG_Pin);
+//    auto sta = HAL_GPIO_ReadPin(BUTTON_TRIG_GPIO_Port, BUTTON_TRIG_Pin);
+
+    if(hreferee.RefereeExist()==false){
+        gimbal_on = true;
+        chassis_on = true;
+    }
 
 
     if(!(chassis_on && gimbal_on))
