86c561ae010b2e80209c33acb2e8ff4b6610d554
Z.Zhou <8520897+zzhouJOJO@user.noreply.gitee.com>
2026-03-28 18:48:03 +0800

    上电遥控器断联修复

--- CppResource/Tasks/RefereeProcessTask.cpp
+++ CppResource/Tasks/RefereeProcessTask.cpp
@@ -11,18 +11,18 @@ using namespace Device;
     while (true){
         href.ProcessData();
 
-
         bool gimbal_on = href.getRefereeInfo<RefereeType::GameRobotState>().power_management_gimbal_output;
         bool frib_on = href.getRefereeInfo<RefereeType::GameRobotState>().power_management_shooter_output;
 
         if(!href.RefereeExist()){
-            gimbal_on = true;
-            frib_on = true;
+//            gimbal_on = true;
+//            frib_on = true;
+            gimbal_on = false;
+            frib_on = false;
         }
 
         HAL_GPIO_WritePin(Frib_Relay_GPIO_Port, Frib_Relay_Pin, frib_on ? GPIO_PIN_SET : GPIO_PIN_RESET);
         HAL_GPIO_WritePin(Pitch_Relay_GPIO_Port, Pitch_Relay_Pin, gimbal_on ? GPIO_PIN_SET : GPIO_PIN_RESET);
-
 
         osDelay(2);
     }
